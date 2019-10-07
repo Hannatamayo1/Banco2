db6db2bceba1a94778f1c780beafb98e6b865025
Hanna Nicolle Tamayo Montes <hanna.tamayo@mariaelvinia.edu.co>
2019-10-07 12:09:53 -0500

    versión 1.0
    Creación de 5 atributos de clase

---
+++ src/banco/Banco.java
@@ -0,0 +1,13 @@
+package banco;
+
+import java.util.Date;
+
+public class Banco {
+
+    private Date fechaDeInauguracion;
+    private boolean tieneSucursales;
+    private String nombreDelGerente;
+    private int numeroDeCajeros;
+    private int numeroDeTrabajadores;
+
+}
